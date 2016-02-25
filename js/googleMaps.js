ae00ea7ca7b379231ae391b5a4ee2fa00d3cd927
victor riveros <hirosmans@gmail.com>
2016-02-25 16:10:56 -0300

    add search address to mobile menu

--- js/googleMaps.js
+++ js/googleMaps.js
@@ -1,52 +1,54 @@
-var map;
-var directionDisplay;
-var directionsService;
 
+	var directionDisplay;
+	var directionsService;
+$(function() {
+	var map;
+	function initialize() {
+		var mapOptions = {
+			center: new google.maps.LatLng(-33.4071127,-70.6071702),
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			disableDefaultUI: true,
+			zoom: 11
+		};
+		directionsService = new google.maps.DirectionsService();
+		directionsDisplay = new google.maps.DirectionsRenderer();
+		map = new google.maps.Map(document.getElementById("map"), mapOptions);
+		directionsDisplay.setMap(map);
 
-function init() {
-	var mapOptions = {
-		center: new google.maps.LatLng(-33.4071127,-70.6071702),
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		disableDefaultUI: true,
-		zoom: 11
-	};
-	directionsService = new google.maps.DirectionsService();
-	directionsDisplay = new google.maps.DirectionsRenderer();
-	map = new google.maps.Map(document.getElementById("map"), mapOptions);
-	directionsDisplay.setMap(map);
+		/* DESKTOP VERSION */
+		var input = (document.getElementById('pac-input'));
+		var types = document.getElementById('type-selector');
+		var autocomplete = new google.maps.places.Autocomplete(input);
 
+		/* MOBILE VERSION */
+		var search = (document.getElementById('search'));
+		var autocompleteMobile = new google.maps.places.Autocomplete(search);
+		
+		autocompleteMobile.bindTo('bounds', map);
+		autocomplete.bindTo('bounds', map);
 
-	var input = /** @type {!HTMLInputElement} */(
-		document.getElementById('pac-input'));
-	var types = document.getElementById('type-selector');
+		var infowindow = new google.maps.InfoWindow();
+		var marker = new google.maps.Marker({
+			map: map,
+			anchorPoint: new google.maps.Point(0, -29)
+		});
 
+		autocomplete.addListener('place_changed', function() {
+			infowindow.close();
+			marker.setVisible(false);
+			var place = autocomplete.getPlace();
+			if (!place.geometry) {
+				window.alert("Autocomplete's returned place contains no geometry");
+				return;
+			};
 
-	var autocomplete = new google.maps.places.Autocomplete(input);
-	autocomplete.bindTo('bounds', map);
-
-	var infowindow = new google.maps.InfoWindow();
-	var marker = new google.maps.Marker({
-		map: map,
-		anchorPoint: new google.maps.Point(0, -29)
-	});
-
-	autocomplete.addListener('place_changed', function() {
-		infowindow.close();
-		marker.setVisible(false);
-		var place = autocomplete.getPlace();
-		if (!place.geometry) {
-			window.alert("Autocomplete's returned place contains no geometry");
-			return;
-		}
-
-
-		if (place.geometry.viewport) {
-			map.fitBounds(place.geometry.viewport);
-		} else {
-			map.setCenter(place.geometry.location);
-				map.setZoom(17);  // Why 17? Because it looks good.
+			if (place.geometry.viewport) {
+				map.fitBounds(place.geometry.viewport);
+			} else {
+				map.setCenter(place.geometry.location);
+				map.setZoom(17);
 			}
-			marker.setIcon(/** @type {google.maps.Icon} */({
+			marker.setIcon(({
 				url: place.icon,
 				size: new google.maps.Size(71, 71),
 				origin: new google.maps.Point(0, 0),
@@ -70,13 +72,42 @@ function init() {
 			});
 		}
 
+		autocompleteMobile.addListener('place_changed', function() {
+			infowindow.close();
+			marker.setVisible(false);
+			var place = autocompleteMobile.getPlace();
+			if (!place.geometry) {
+				window.alert("Autocomplete's returned place contains no geometry");
+				return;
+			};
 
+			if (place.geometry.viewport) {
+				map.fitBounds(place.geometry.viewport);
+			} else {
+				map.setCenter(place.geometry.location);
+				map.setZoom(17);
+			};
+			marker.setIcon(({
+				url: place.icon,
+				size: new google.maps.Size(71, 71),
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(17, 34),
+				scaledSize: new google.maps.Size(35, 35)
+			}));
+			marker.setPosition(place.geometry.location);
+			marker.setVisible(true);
+
+			var address = '';
+			infowindow.setContent('<div><strong>' + place.name + '</strong><p>' + address+'<p> <div class="card-action"><a onClick="getRouteToStore('+place.geometry.location.lat()+', '+place.geometry.location.lng()+')">Como llegar</a></div></div>');
+			infowindow.open(map, marker);
+		});
 	};
+	initialize();
 
+	$('.button-collapse').sideNav();
+});
 
-
-	function getMyPosition()
-	{
+	function getMyPosition() {
 		var infoWindow = new google.maps.InfoWindow({map: map});
 		if (navigator.geolocation) {
 			navigator.geolocation.getCurrentPosition(function(position) {
@@ -91,16 +122,14 @@ function init() {
 				handleLocationError(true, infoWindow, map.getCenter());
 			});
 		} else {
-			// Browser doesn't support Geolocation
 			handleLocationError(false, infoWindow, map.getCenter());
-		}
+		};
 	};
 
 	var getRouteToStore = function(latDestination, longDestination,latOrigin, longOrigin ) {
-		if(latOrigin==0 || longOrigin==0)
-		{
+		if(latOrigin==0 || longOrigin==0) {
 			return false;
-		}
+		};
 
 		var request = {
 			origin: new google.maps.LatLng('-33.5537001', '-70.631175'), // central or current position
@@ -127,10 +156,4 @@ function init() {
 			'Error: The Geolocation service failed.' :
 			'Error: Your browser doesn\'t support geolocation.');
 	};
-
-
-$(function() {
-	$('.button-collapse').sideNav();
-});
-
 
--- js/order_detail.js
+++ js/order_detail.js
@@ -1,4 +1,5 @@
 
+$(function() {
 /*
 ======================
 	MAP DEFINITION
@@ -6,9 +7,7 @@
 */
 var map;
 var directionDisplay;
-//var directionsService = new google.maps.DirectionsService();
-
-
+var directionsService;
 function initialize() {
 	var mapOptions = {
 		center: new google.maps.LatLng(-33.4071127,-70.6071702),
@@ -16,134 +15,128 @@ function initialize() {
 		disableDefaultUI: true,
 		zoom: 11
 	};
+	directionsService = new google.maps.DirectionsService();
+	directionsDisplay = new google.maps.DirectionsRenderer();
 	map = new google.maps.Map(document.getElementById("map"), mapOptions);
-	
+	directionsDisplay.setMap(map);
 };
 
-$('.button-collapse').sideNav('show');
-$('.button-collapse').sideNav('hide');
-$(document).ready(function(){
+var orders = [{
+	id: 1,
+	customerId: 1,
+	customerName: 'Bazar uno',
+	products: [
+	{ productName: 'fruttare', quantity: 20, totalAmount: 7000 },
+	{ productName: 'viennetta', quantity: 20, totalAmount: 14000 },
+	{ productName: 'choco', quantity: 30, totalAmount: 10000 },
+	{ productName: 'brigadeiro', quantity: 10, totalAmount: 5000 },
+	],
+	coords: { latitude: -33.4622355, longitude: -70.7033767 }
+},	{
+	id: 2,
+	customerId: 2,
+	customerName: 'Bazar dos',
+	products: [
+	{ productName: 'fruttare', quantity: 20, totalAmount: 7000 },
+	{ productName: 'viennetta', quantity: 20, totalAmount: 14000 },
+	{ productName: 'choco', quantity: 30, totalAmount: 10000 },
+	{ productName: 'brigadeiro', quantity: 10, totalAmount: 5000 },
+	],
+	coords: { latitude: -33.4621815, longitude: -70.7024537 }
+},	{
+	id: 3,
+	customerId: 3,
+	customerName: 'Bazar tres',
+	products: [
+	{ productName: 'fruttare', quantity: 20, totalAmount: 7000 },
+	{ productName: 'viennetta', quantity: 20, totalAmount: 14000 },
+	{ productName: 'choco', quantity: 30, totalAmount: 10000 },
+	{ productName: 'brigadeiro', quantity: 10, totalAmount: 5000 },
+	],
+	coords: { latitude: -33.4620385, longitude: -70.7043417 }
+},	{
+	id: 4,
+	customerId: 1,
+	customerName: 'Bazar cuatro',
+	products: [
+	{ productName: 'fruttare', quantity: 20, totalAmount: 7000 },
+	{ productName: 'viennetta', quantity: 20, totalAmount: 14000 },
+	{ productName: 'choco', quantity: 30, totalAmount: 10000 },
+	{ productName: 'brigadeiro', quantity: 10, totalAmount: 5000 },
+	],
+	coords: { latitude: -33.4619485, longitude: -70.7052107 }
+},	{
+	id: 5,
+	customerId: 2,
+	customerName: 'Bazar cinco',
+	products: [
+	{ productName: 'fruttare', quantity: 20, totalAmount: 7000 },
+	{ productName: 'viennetta', quantity: 20, totalAmount: 14000 },
+	{ productName: 'choco', quantity: 30, totalAmount: 10000 },
+	{ productName: 'brigadeiro', quantity: 10, totalAmount: 5000 },
+	],
+	coords: { latitude: -33.4639805, longitude: -70.7071097 }
+},	{
+	id: 6,
+	customerId: 3,
+	customerName: 'Bazar seis',
+	products: [
+	{ productName: 'fruttare', quantity: 20, totalAmount: 7000 },
+	{ productName: 'viennetta', quantity: 20, totalAmount: 14000 },
+	{ productName: 'choco', quantity: 30, totalAmount: 10000 },
+	{ productName: 'brigadeiro', quantity: 10, totalAmount: 5000 },
+	],
+	coords: { latitude: -33.4627185, longitude: -70.7074857 }
+}];
 
-	var orders = [
-	{
-		id: 1,
-		customerId: 1,
-		customerName: 'Bazar uno',
-		products: [
-		{ productName: 'fruttare', quantity: 20, totalAmount: 7000 },
-		{ productName: 'viennetta', quantity: 20, totalAmount: 14000 },
-		{ productName: 'choco', quantity: 30, totalAmount: 10000 },
-		{ productName: 'brigadeiro', quantity: 10, totalAmount: 5000 },
-		],
-		coords: { latitude: -33.4622355, longitude: -70.7033767 }
-	},	{
-		id: 2,
-		customerId: 2,
-		customerName: 'Bazar dos',
-		products: [
-		{ productName: 'fruttare', quantity: 20, totalAmount: 7000 },
-		{ productName: 'viennetta', quantity: 20, totalAmount: 14000 },
-		{ productName: 'choco', quantity: 30, totalAmount: 10000 },
-		{ productName: 'brigadeiro', quantity: 10, totalAmount: 5000 },
-		],
-		coords: { latitude: -33.4621815, longitude: -70.7024537 }
-	},	{
-		id: 3,
-		customerId: 3,
-		customerName: 'Bazar tres',
-		products: [
-		{ productName: 'fruttare', quantity: 20, totalAmount: 7000 },
-		{ productName: 'viennetta', quantity: 20, totalAmount: 14000 },
-		{ productName: 'choco', quantity: 30, totalAmount: 10000 },
-		{ productName: 'brigadeiro', quantity: 10, totalAmount: 5000 },
-		],
-		coords: { latitude: -33.4620385, longitude: -70.7043417 }
-	},	{
-		id: 4,
-		customerId: 1,
-		customerName: 'Bazar cuatro',
-		products: [
-		{ productName: 'fruttare', quantity: 20, totalAmount: 7000 },
-		{ productName: 'viennetta', quantity: 20, totalAmount: 14000 },
-		{ productName: 'choco', quantity: 30, totalAmount: 10000 },
-		{ productName: 'brigadeiro', quantity: 10, totalAmount: 5000 },
-		],
-		coords: { latitude: -33.4619485, longitude: -70.7052107 }
-	},	{
-		id: 5,
-		customerId: 2,
-		customerName: 'Bazar cinco',
-		products: [
-		{ productName: 'fruttare', quantity: 20, totalAmount: 7000 },
-		{ productName: 'viennetta', quantity: 20, totalAmount: 14000 },
-		{ productName: 'choco', quantity: 30, totalAmount: 10000 },
-		{ productName: 'brigadeiro', quantity: 10, totalAmount: 5000 },
-		],
-		coords: { latitude: -33.4639805, longitude: -70.7071097 }
-	},	{
-		id: 6,
-		customerId: 3,
-		customerName: 'Bazar seis',
-		products: [
-		{ productName: 'fruttare', quantity: 20, totalAmount: 7000 },
-		{ productName: 'viennetta', quantity: 20, totalAmount: 14000 },
-		{ productName: 'choco', quantity: 30, totalAmount: 10000 },
-		{ productName: 'brigadeiro', quantity: 10, totalAmount: 5000 },
-		],
-		coords: { latitude: -33.4627185, longitude: -70.7074857 }
-	}];
+var loadOrders = function() {
 
-	var loadOrders = function() {
-
-		$.each(orders, function(index, order) {
-			var products = '';
-			products =  '<table class="highlight">'
-			+	'	<tr>'
-			+	'		<th data-field="products">producto</th>'
-			+	'		<th data-field="quantity">cantidad</th>'
-			+	'		<th data-field="total">total</th>'
-			+ ' </tr>'
-			$.each(order.products, function(index, product) {
-				products += '<tr>'
-				+	'	<td>'+ product.productName + '</td>'
-				+	'	<td>'+ product.quantity + '</td>'
-				+	'	<td>'+ product.totalAmount + '</td>'
-				+ '</tr>';
-			});
-			products +=	'</table>';
-
-			$('#custom-container').append('<div class="card">'
-				+ 	'<div class="card-image waves-effect waves-block waves-light">'
-				+ 	'</div>'
-				+ 	'<div class="card-content">'
-				+			'<span class="card-title activator grey-text text-darken-4">' + order.customerName + '<i class="material-icons right">more_vert</i></span>'
-				+		'	<p class="text-left"><b>coordenadas: </b><br />'
-				+ 	'&nbsp;&nbsp;latitud: ' + order.coords.latitude + '<p> &nbsp;&nbsp;longitud ' + order.coords.longitude + '</p>'
-				+		'&nbsp;&nbsp;<a href="#" class="btn-route" data-latitude="' + order.coords.latitude + '" data-longitude="' + order.coords.longitude + '">ver ruta</a>'
-				+ 	'</div>'
-				+ 	'<div class="card-reveal">'
-				+			'<span class="card-title grey-text text-darken-4">' + order.customerName + '<i class="material-icons right">close</i></span>'
-				+			products
-				+ 	'</div>'
-				+ '</div>'
-				);
+	$.each(orders, function(index, order) {
+		var products = '';
+		products =  '<table class="highlight">'
+		+	'	<tr>'
+		+	'		<th data-field="products">producto</th>'
+		+	'		<th data-field="quantity">cantidad</th>'
+		+	'		<th data-field="total">total</th>'
+		+ ' </tr>'
+		$.each(order.products, function(index, product) {
+			products += '<tr>'
+			+	'	<td>'+ product.productName + '</td>'
+			+	'	<td>'+ product.quantity + '</td>'
+			+	'	<td>'+ product.totalAmount + '</td>'
+			+ '</tr>';
 		});
-	};
-	loadOrders();
+		products +=	'</table>';
 
-	$('.btn-route').click(function() {
-
-		if ($(this).data('latitude') != '' && $(this).data('longitude') != '') {
-
-			$('#directions').empty();
-			$('#collapsible-container').removeClass('hide');
-
-
-			getRouteToStore($(this).data('latitude'), $(this).data('longitude'));
-		} else {
-			alert('no se han ingresado las coordenadas');
-		};
+		$('#custom-container').append('<div class="card">'
+			+ 	'<div class="card-image waves-effect waves-block waves-light">'
+			+ 	'</div>'
+			+ 	'<div class="card-content">'
+			+			'<span class="card-title activator grey-text text-darken-4">' + order.customerName + '<i class="material-icons right">more_vert</i></span>'
+			+		'	<p class="text-left"><b>coordenadas: </b><br />'
+			+ 	'&nbsp;&nbsp;latitud: ' + order.coords.latitude + '<p> &nbsp;&nbsp;longitud ' + order.coords.longitude + '</p>'
+			+		'&nbsp;&nbsp;<a href="#" class="btn-route" data-latitude="' + order.coords.latitude + '" data-longitude="' + order.coords.longitude + '">ver ruta</a>'
+			+ 	'</div>'
+			+ 	'<div class="card-reveal">'
+			+			'<span class="card-title grey-text text-darken-4">' + order.customerName + '<i class="material-icons right">close</i></span>'
+			+			products
+			+ 	'</div>'
+			+ '</div>'
+			);
 	});
+};
+loadOrders();
+
+$('.btn-route').click(function() {
+
+	if ($(this).data('latitude') != '' && $(this).data('longitude') != '') {
+		$('#directions').empty();
+		$('#collapsible-container').removeClass('hide');
+		getRouteToStore($(this).data('latitude'), $(this).data('longitude'));
+	} else {
+		alert('no se han ingresado las coordenadas');
+	};
+});
 
 
 
